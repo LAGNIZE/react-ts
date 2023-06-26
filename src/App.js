40b3da66e2844c2261e48f937a5a2cfe843ce887
Aleksei Demichev <a.demichev@potok.team>
2023-06-26 14:42:40 +0300

    React JS fundamentals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'a', body: 'c' },
+    { id: 2, title: 'b', body: 'a' },
+    { id: 3, title: 'c', body: 'b' },
+  ]);
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const getSortedPosts = () => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  };
+
+  const sortedPosts = getSortedPosts();
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const removePost = (post) => {
+    setPosts(posts.filter((el) => el.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost} />
+      <hr />
+      <div>
+        <MyInput
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Searching..."
+        />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Sort by"
+          options={[
+            {
+              value: 'title',
+              name: 'Name',
+            },
+            {
+              value: 'body',
+              name: 'Desc',
+            },
+          ]}
+        />
+      </div>
+      {posts.length !== 0 ? (
+        <PostList
+          remove={removePost}
+          posts={sortedPosts}
+          title="List number 1"
+        />
+      ) : (
+        <h1>Not Found</h1>
+      )}
     </div>
   );
 }
